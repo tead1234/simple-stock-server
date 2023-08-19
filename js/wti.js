c931f502aed7583e114a0cebe9a2578c1e1409ee
jp <tead1234@gmail.com>
2023-08-19 14:26:57 +0000

    promise 처리

--- js/wti.js
+++ js/wti.js
@@ -7,6 +7,8 @@ const url = 'https://www.cnbc.com/quotes/@CL.1';
 const target = '#quote-page-strip > div.QuoteStrip-dataContainer > div.QuoteStrip-lastTimeAndPriceContainer > div.QuoteStrip-lastPriceStripContainer > span.QuoteStrip-lastPrice';
 const getWti = new Crawler(url, target );
 
-let a = getWti.start();
+let a = getWti.start().then((sucess) => {
+    return sucess;
+})
 module.exports = { a };
   
